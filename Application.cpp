56cc71f0ffa5bf2c9704750670d8275991038c51
Brad Flubacher <brad@techsoft3d.com>
2021-07-28 12:42:43 -0400

    Added code to initialize and terminate exchange.

--- Application.cpp
+++ Application.cpp
@@ -1,3 +1,6 @@
+#define INITIALIZE_A3D_API
+#include <A3DSDKIncludes.h>
+
 #include "Application.h"
 #include "MainWindow.h"
 #include "hoops_license.h"
@@ -11,6 +14,17 @@ _world(HOOPS_LICENSE) {
     setApplicationDisplayName("HOOPS Exchange Tutorial");
     
     // Initialize HOOPS Exchange
+    if(!A3DSDKLoadLibrary("/opt/local/ts3d/HOOPS_Exchange_2021_SP1_U1/bin/osx64")) {
+        throw std::runtime_error("Unable to load HOOPS Exchange.");
+    }
+
+    if(A3D_SUCCESS != A3DLicPutUnifiedLicense(HOOPS_LICENSE)) {
+        throw std::runtime_error("Invalid HOOPS Exchange license.");
+    }
+
+    if(A3D_SUCCESS != A3DDllInitialize(A3D_DLL_MAJORVERSION, A3D_DLL_MINORVERSION)) {
+        throw std::runtime_error("HOOPS Exchange version mismatch.");
+    }
 
     
     _main_window = new MainWindow();
@@ -19,4 +33,6 @@ _world(HOOPS_LICENSE) {
 
 Application::~Application(void) {
     // Terminate HOPOS Exchange
+    A3DDllTerminate();
+    A3DSDKUnloadLibrary();
 }
